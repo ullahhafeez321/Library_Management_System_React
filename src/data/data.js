ed0e63d52a391519ed5999f32cc7eefc5c9b6455
ullahhafeez321 <ullahhafeez321@gmail.com>
2025-02-08 10:45:15 +0500

    minor changes in data

--- src/data/data.js
+++ src/data/data.js
@@ -3,14 +3,14 @@ const initialBooks = [
     id: 1,
     book: "Alif",
     genere: "Novel",
-    author: "Hafeez",
+    author: "Humaira Ahmed",
     status: "Available",
   },
   {
     id: 2,
-    book: "Men's Searching For Meaning",
+    book: "Man's Search For Meaning",
     genere: "Story",
-    author: "Hamza Assa",
+    author: "Viktor E. Frankl",
     status: "Borrowed",
   },
 ];
